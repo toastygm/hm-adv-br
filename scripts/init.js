a87bd0bf60a2b467c1eecb795130de3c395570ce
Tom Rodriguez <tmrodrig@gmail.com>
2022-08-06 16:24:42 -0700

    Changed welcome journal

--- scripts/init.js
+++ scripts/init.js
@@ -7,7 +7,7 @@
    * Set to the following to disable:
    *   const welcomeJournal = '';
    */
-  const welcomeJournal = '_Overview';
+  const welcomeJournal = 'Bloody Raven';
   /**
    * additionalJournals will automatically be imported.
    * Set to the following to disable:
